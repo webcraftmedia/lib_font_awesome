895ccfb0b94ee4c2242ea4dd211f24bc83f3eb53
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-19 21:54:07 +0100

    new caching system implemented, updated system reference

--- lib_font_awesome.php
+++ lib_font_awesome.php
@@ -4,7 +4,7 @@ class lib_font_awesome extends \LIB\lib_css{
     public static function get_class(){
         return self::class;}
     public static function css_path(){
-        return \SYSTEM\WEBPATH(new \PLIB(),'font_awesome/lib/css/font-awesome.min.css');}
+        return new \PLIB('font_awesome/lib/css/font-awesome.min.css');}
     public static function version(){
         return '<a href="https://github.com/FortAwesome/Font-Awesome/commit/5ad6a98a54873dfdbadb0639405cdb0660bc6092">github commit</a>';}
 }
