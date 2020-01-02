bd0fdef16825d10ccbcccafbf083ef24e05aed75
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2020-01-02 15:06:38 +0100

    adjusted include + commit id

--- lib_font_awesome.php
+++ lib_font_awesome.php
@@ -4,7 +4,7 @@ class lib_font_awesome extends \LIB\lib_css{
     public static function get_class(){
         return self::class;}
     public static function css_path(){
-        return new \SYSTEM\PLIB('font_awesome/lib/css/font-awesome.min.css');}
+        return new \SYSTEM\PLIB('font_awesome/lib/css/all.min.css');}
     public static function version(){
-        return '<a href="https://github.com/FortAwesome/Font-Awesome/commit/8e8e659341916ad8a551835c43b30395cd90f489">commit 8e8e659341916ad8a551835c43b30395cd90f489</a>';}
+        return '<a href="https://github.com/FortAwesome/Font-Awesome/commit/d8db3d032d22e7a5e21f3b40905b7867be19bb02">commit d8db3d032d22e7a5e21f3b40905b7867be19bb02</a>';}
 }
