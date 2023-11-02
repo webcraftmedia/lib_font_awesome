29f91b4052222205d8ea11254ff18d42e142f1d8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-02 20:15:54 +0100

    font_awesome v4

--- lib_font_awesome4.php
+++ lib_font_awesome4.php
@@ -1,10 +1,10 @@
 <?php
 namespace LIB;
-class lib_font_awesome extends \LIB\lib_css{
+class lib_font_awesome4 extends \LIB\lib_css{
     public static function get_class(){
         return self::class;}
     public static function css_path(){
-        return new \PLIB('font_awesome/lib/css/font-awesome.min.css');}
+        return new \PLIB('font_awesome4/lib/css/font-awesome.min.css');}
     public static function version(){
         return '<a href="https://github.com/FortAwesome/Font-Awesome/commit/5ad6a98a54873dfdbadb0639405cdb0660bc6092">github commit</a>';}
 }
